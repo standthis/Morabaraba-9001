0febe848850019df40858f134a7589a32049ed35
standthis <g14d5954@HUG182>
2018-04-15 19:40:11 +0200

    First test - EmptyBoard

--- Morabaraba-9001.Test/Tests.cs
+++ Morabaraba-9001.Test/Tests.cs
@@ -1,9 +1,45 @@
 ﻿using System;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+using NSubstitute;
+using Morabaraba_9001;
 
 namespace Morabaraba_9001.Test
 {
+    [TestFixture]
     public class Tests
     {
+        [Test]
+        public void ABoardHas24EmptyTiles()
+        {
+            Board b = new Board();
+            Assert.That(b.allTiles.Values.Where(tile => tile.Cow != null).Count() == 0);
+        }
+
+        [Test]
+        public void ThePlayerWithDarkCowsMovesFirst()
+        {
+
+        }
+
+        [Test]
+        public void CowsCanOnlyBePlacesOnEmptySpaces()
+        {
+
+        }
+
+        [Test]
+        public void AMaximumOf12PlacementsPerPlayer()
+        {
+
+        }
+
+        [Test]
+        public void CowsCannotBeMovedDuringPlacement()
+        {
+
+        }
 
     }
 }
