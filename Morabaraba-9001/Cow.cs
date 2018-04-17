0cd0fead5ae1ab69b8f304d68bd79b82bfa018d2
louise-poole <louisecarmenpoole@gmail.com>
2018-04-17 19:06:28 +0200

    set Cow constructor used for testing to only need a position to be created (and is set as having been placed on the board)

--- Morabaraba-9001/Cow.cs
+++ Morabaraba-9001/Cow.cs
@@ -11,6 +11,12 @@ namespace Morabaraba_9001
             status = cowStatus.Unplaced;
         }
 
+        public Cow((char, int) posi)//used for testing: creates a cow already placed at a given position
+        {
+            pos = posi;
+            status = cowStatus.Placed;
+        }
+
         public Color Color { get; set; }
         public (char, int) pos { get; set; }
         public cowStatus status { get; set; }
