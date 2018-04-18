72ae46c884fd5b346195a98ccd54da562ecbd258
Martin Simango <Martin@Martins-MacBook-Pro.local>
2018-04-18 09:19:35 +0200

    complete possible moves test

--- Morabaraba-9001.Test/Tests.cs
+++ Morabaraba-9001.Test/Tests.cs
@@ -12,7 +12,7 @@ namespace Morabaraba_9001.Test
     {
 
         //TESTS FOR DURING PLACEMENT
-        
+
 
         static object[] allBoardPositions =
         {
@@ -40,7 +40,7 @@ namespace Morabaraba_9001.Test
             new object[] { ('G', 1) },
             new object[] { ('G', 4) },
             new object[] { ('G', 7) },
-               
+
         };
         [Test]
         [TestCaseSource(nameof(allBoardPositions))]
@@ -62,10 +62,10 @@ namespace Morabaraba_9001.Test
             lightPlayer.Color.Returns(Color.light);
 
             IBoard b = Substitute.For<IBoard>();//create mock board
-            
+
             IReferee ref1 = new Referee(darkPlayer, lightPlayer, b);//create 2 refs (game instances) in only the 2 possible ways a ref can be created with the same 2 players
             IReferee ref2 = new Referee(lightPlayer, darkPlayer, b);
-            
+
             Assert.That(ref1.CurrentPlayer.Color == Color.dark);//check that for both refs, the CurrentPlayer (starting player as no moves have been made) is the dark one
             Assert.That(ref2.CurrentPlayer.Color == Color.dark);
         }
@@ -174,7 +174,7 @@ namespace Morabaraba_9001.Test
         {
             IPlayer player;
             IReferee mockReferee = Substitute.For<IReferee>();
-           
+
             MoveError result;
             if (isOpenBoardSpace)
             {
@@ -182,7 +182,7 @@ namespace Morabaraba_9001.Test
                 mockReferee.Place(player, pos).Returns(MoveError.Valid);
                 result = player.placeCow(pos, mockReferee);
 
-                Assert.That(player.Cows.Where(x => x.pos.Equals(pos)).Count()==1);
+                Assert.That(player.Cows.Where(x => x.pos.Equals(pos)).Count() == 1);
                 Assert.That(result == expected);
             }
             else
@@ -193,32 +193,32 @@ namespace Morabaraba_9001.Test
                 mockReferee.Place(player, pos).Returns(MoveError.InValid);
                 result = player.placeCow(pos, mockReferee);
 
-               //check that player still has cow
+                //check that player still has cow
                 Assert.That(player.Cows.Where(x => x.pos.Equals(pos)).Count() == 1);
                 Assert.That(result == expected); //was an invalid move
             }
-            
 
 
-             //try place a cow in the given position
 
-          
-            
+            //try place a cow in the given position
+
+
+
 
             //IPlayer player = Substitute.For<IPlayer>();
             //IBoard board = new Board(); //Substitute.For<IBoard>();
             //if (isOpenBoardSpace == true)
             //{
-                //Dictionary<(char, int), ITile> mocked = new Dictionary<(char, int), ITile>();
-                //ITile tileMock = Substitute.For<ITile>();
-                //tileMock.Cow.Returns((ICow)null);
-                //mocked[pos] = tileMock;
-                //board.AllTiles.Returns(mocked);
-                //board.AllTiles[pos].Cow.Returns((ICow)null);
+            //Dictionary<(char, int), ITile> mocked = new Dictionary<(char, int), ITile>();
+            //ITile tileMock = Substitute.For<ITile>();
+            //tileMock.Cow.Returns((ICow)null);
+            //mocked[pos] = tileMock;
+            //board.AllTiles.Returns(mocked);
+            //board.AllTiles[pos].Cow.Returns((ICow)null);
             //}
             //else
             //{
-               // board.AllTiles[pos].Cow.Returns(Arg.Any<ICow>());
+            // board.AllTiles[pos].Cow.Returns(Arg.Any<ICow>());
             //}
             //MoveError result = board.PlaceCow(player, pos);
             //Assert.That(result == expected);
@@ -244,30 +244,60 @@ namespace Morabaraba_9001.Test
             IBoard b = Substitute.For<IBoard>();
             IPlayer p1 = Substitute.For<IPlayer>();
             p1.Color.Returns(Color.dark);
-            p1.State.Returns(PlayerState.Placing);            
+            p1.State.Returns(PlayerState.Placing);
             IPlayer p2 = Substitute.For<IPlayer>();
             p2.Color.Returns(Color.light);
             IReferee myRef = new Referee(p1, p2, b);
             MoveError result = myRef.Move(p1, ('A', 0), ('A', 0));
             Assert.AreEqual(result, MoveError.InValid);
         }
-        
+
         //TESTS FOR DURING MOVING
         // Incomplete 
+        static object[] connectedSpaceTest = {
+        new object [] {('A', 1), new List<(char, int)> { ('A', 4), ('B', 2), ('D', 1) }},
+        new object [] {('A', 4), new List<(char, int)> { ('A', 1), ('A', 7), ('B', 4) }},
+        new object [] {('A', 7), new List<(char, int)> { ('A', 4), ('B', 6), ('D', 7) }},
 
+        new object [] {('B', 2), new List<(char, int)> { ('A', 1), ('B', 4), ('C', 3),('D', 2) }},
+        new object [] {('B', 4), new List<(char, int)> { ('A', 4), ('B', 2), ('B', 6),('C', 4) }},
+        new object [] {('B', 6), new List<(char, int)> { ('A', 7), ('B', 4), ('D', 6),('C', 5) }},
+
+
+        new object [] {('C', 3), new List<(char, int)> { ('B', 2), ('C', 4), ('D', 3) }},
+        new object [] {('C', 4), new List<(char, int)> { ('B', 4), ('C', 3), ('C', 5) }},
+        new object [] {('C', 5), new List<(char, int)> { ('B', 6), ('C', 4), ('D', 5) }},
+
+        new object [] {('D', 1), new List<(char, int)> { ('A', 1), ('D', 2), ('G', 1) }},
+        new object [] {('D', 2), new List<(char, int)> { ('B', 2), ('D', 1), ('D', 3),('F', 2) }},
+        new object [] {('D', 3), new List<(char, int)> { ('C', 3), ('D', 2), ('E', 3) }},
+
+        new object [] {('D', 5), new List<(char, int)> { ('C', 5), ('D', 6), ('E', 5) }},
+        new object [] {('D', 6), new List<(char, int)> { ('B', 6), ('D', 5), ('D', 7),('F', 6) }},
+        new object [] {('D', 7), new List<(char, int)> { ('A', 7), ('D', 6), ('G', 7) }},
+
+        new object [] {('E', 3), new List<(char, int)> { ('A', 7), ('D', 6), ('G', 7) }},
+        new object [] {('E', 4), new List<(char, int)> { ('E', 3), ('F', 4), ('E', 5) }},
+        new object [] {('E', 5), new List<(char, int)> { ('D', 5), ('E', 4), ('F', 6) }},
+
+        new object [] {('F', 2), new List<(char, int)> { ('D', 2), ('E', 3), ('F', 4), ('G', 1) }},
+        new object [] {('F', 4), new List<(char, int)> { ('E', 4), ('F', 2), ('F', 6), ('G', 4) }},
+        new object [] {('F', 6), new List<(char, int)> { ('D', 6), ('E', 5), ('F', 4), ('G', 7) }},
+
+        new object [] {('G', 1), new List<(char, int)> { ('D', 1), ('F', 2), ('G', 4) }},
+        new object [] {('G', 4), new List<(char, int)> { ('F', 4), ('G', 1), ('G', 7) }},
+        new object [] {('G', 7), new List<(char, int)> { ('D', 7), ('F', 6), ('G', 4) }},
+         
+
+        };
         
         [Test]
-        public void ACowCanOnlyMoveToAnotherConnectedSpace(Color c, (char,int) pos) // matt 
+        [TestCaseSource(nameof(connectedSpaceTest))]
+        public void ACowCanOnlyMoveToAnotherConnectedSpace((char,int) pos,List<(char,int)> expected) // matt 
         {
-            ICow cow = new Cow(c);
-            IBoard b = Substitute.For<IBoard>();
-            IPlayer p = new Player("player", c);
-
-            (char,int)[] posMoves = b.AllTiles[pos].PossibleMoves.ToArray();
-            foreach ((char,int) tile in posMoves){
-
-            }
-//            b.Cows(c).Returns(new ITile[] {  });  
+            IBoard b = new Board();
+            Assert.AreEqual(b.PossibleMoves(pos), expected);
+ 
 
         }
 
@@ -347,7 +377,7 @@ namespace Morabaraba_9001.Test
         {
             IReferee mockReferee = Substitute.For<IReferee>();
             Player player = new Player("test player", Color.dark);//create an enemy player (the player being shot)
-
+            mockReferee.KillCow(player,pos).Returns(MoveError.InValid);
             MoveError result = player.killCow(pos, mockReferee);//request for enemy player's cow to be killed at the position where no cow has been created for
             Assert.That(result != MoveError.Valid);
         }
