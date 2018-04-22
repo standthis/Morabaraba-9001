5f2077b16109e18a5f1f8beebd4dd7787a801603
Martin Simango <Martin@Martins-MacBook-Pro.local>
2018-04-22 15:17:28 +0200

    add testing for printing board

--- Morabaraba-9001/Program.cs
+++ Morabaraba-9001/Program.cs
@@ -10,21 +10,9 @@ namespace Morabaraba_9001
             static void Main(string[] args)
             {
                 Console.WriteLine("Morabaraba!");
-            List<(char, int)> posList = new List<(char, int)>();
-            posList.Add(('A', 1));
-            posList.Add(('A', 4));
-            posList.Add(('A', 7));
-            Player player = new Player("h", Color.dark, PlayerState.Flying, posList);
-            Board board = new Board();
-            //Console.WriteLine(board.MillFormed(player, ('A', 7)));
-            foreach(List<ITile> mill in board.AllBoardMills){
-                foreach(ITile tile in mill){
-                    Console.Write(tile.Pos+" ");
-                }
-                Console.WriteLine();
-            }
-           
-               // Board b = new Board();
+                    
+                Board b = new Board();
+            b.printBoard();
                 //Console.WriteLine(b.allTiles.Values.Where(tile => tile.Cow == null).Count());
                 //Console.ReadKey();
             }
